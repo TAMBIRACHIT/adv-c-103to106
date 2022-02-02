8d8556dd6925d00b38b5a329f6d613f0d2ca599b
coder519 <95204792+Coder519@users.noreply.github.com>
2022-02-02 22:09:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+Webcam.set({
+          width:350,
+          height:300,
+          image_format : 'png',
+          png_quality:90
+        });
+      
+        camera = document.getElementById("camera");
+      
+      Webcam.attach( '#camera' );
+      
+            
+      function take_snapshot()
+      {
+          Webcam.snap(function(data_uri) {
+              document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+          });
+      }
+      
+      console.log('ml5 version:', ml5.version);
+        
+      
+      classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7FsbsMheK/model.json',modelLoade);
+        function modelLoaded() {
+          console.log('Model Loaded!');
+        }
+            
